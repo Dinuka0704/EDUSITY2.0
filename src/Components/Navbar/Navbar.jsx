c81b748993d5e25c0311341fc5ea275f89d0d27b
Dinuka0704 <dinuka.gimhana2001@gmail.com>
2025-03-07 22:43:54 +0530

    nav bar
    07/03/2025

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 
 function Navbar() {
   return (
-    <nav>
-      <img src="" alt="" />
+    <nav className='container'>
+      <img src={logo} alt="LOGO" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimontals</li>
+        <li><button className='btn'>Contact Us</button></li>
+      </ul>
     </nav>
   )
 }
