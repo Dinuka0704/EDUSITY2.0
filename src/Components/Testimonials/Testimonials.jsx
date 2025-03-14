25cf0c014a1f7187d054ef6a08365c38df082584
Dinuka0704 <dinuka.gimhana2001@gmail.com>
2025-03-14 23:53:04 +0530

    slider1.0
    14/03/2025

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -13,7 +13,70 @@ const Testimonials = () => {
     <div className='container testimonial'>
       <img src={next_icon} alt="" className='next-btn'/>
       <img src={back_icon} alt="" className='back-btn'/>
-      <div className="slider"></div>
+      <div className="slider">
+        <ul>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias repellat esse, tempora corporis saepe maiores rerum ex consequatur ipsum ratione. Exercitationem numquam minima natus iusto alias est at maxime omnis.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias repellat esse, tempora corporis saepe maiores rerum ex consequatur ipsum ratione. Exercitationem numquam minima natus iusto alias est at maxime omnis.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias repellat esse, tempora corporis saepe maiores rerum ex consequatur ipsum ratione. Exercitationem numquam minima natus iusto alias est at maxime omnis.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias repellat esse, tempora corporis saepe maiores rerum ex consequatur ipsum ratione. Exercitationem numquam minima natus iusto alias est at maxime omnis.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias repellat esse, tempora corporis saepe maiores rerum ex consequatur ipsum ratione. Exercitationem numquam minima natus iusto alias est at maxime omnis.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
