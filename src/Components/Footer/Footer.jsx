8b10baa0b077c33a37023ad204270006b359c069
Dinuka0704 <dinuka.gimhana2001@gmail.com>
2025-03-16 19:24:38 +0530

    footer section
    16/03/2025

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer container'>
+      <p>© 2025 Educity. All rights reserved.</p>
+      <ul>
+        <li>Terms of services</li>
+        <li>Privacy policy</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
