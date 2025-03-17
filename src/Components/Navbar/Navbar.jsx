bea7f22ecb7d77fe39949427ffb474d74d55f416
Dinuka0704 <dinuka.gimhana2001@gmail.com>
2025-03-17 16:31:19 +0530

    media queries1.0
    17/03/2025

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-scroll";
+import menu_icon from '../../assets/menu-icon.png';
 
 function Navbar() {
   const [sticky, setSticky] = useState(false);
@@ -11,10 +12,15 @@ function Navbar() {
     });
   }, []);
 
+  const [mobileMenu,setMobileMenu] = useState(false);
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="LOGO" className="logo" />
-      <ul>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><Link to="hero" smooth={true} offset={0} duration={800}> Home </Link> </li>
         <li><Link to="program" smooth={true} offset={-260} duration={800}>Program</Link></li>
         <li><Link to="about" smooth={true} offset={-150} duration={800}>About us</Link></li>
@@ -24,6 +30,7 @@ function Navbar() {
           <Link to="contact" smooth='true' offset={-260} duration={800} className="btn">Contact Us</Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu}/>
     </nav>
   );
 }
